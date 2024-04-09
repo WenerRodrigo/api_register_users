e65371a904a14b0a0052aa0d4847927c32d53799
WenerRodrigo <wener19.wr@gmail.com>
2024-04-08 22:02:13 -0300

    chore: separating routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import fastify from "fastify";
-import { z } from "zod";
 import {
   serializerCompiler,
   validatorCompiler,
-  ZodTypeProvider,
 } from "fastify-type-provider-zod";
-import { PrismaClient } from "@prisma/client";
-import { generateSlug } from "./utils/generate-slug";
 import dotenv from "dotenv";
+import { create } from "domain";
+import { createEvent } from "./routes/create.event";
 
 dotenv.config();
 
@@ -16,53 +14,7 @@ const app = fastify();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
-
-app.withTypeProvider<ZodTypeProvider>().post(
-  "/events",
-  {
-    schema: {
-      body: z.object({
-        title: z.string().min(1).max(255),
-        details: z.string().min(1).max(255),
-        maximumAttendees: z.number().int().positive(),
-      }),
-      response: {
-        201: z.object({
-          eventId: z.string().uuid(),
-        })
-      }
-    },
-  },
-  async (request, reply) => {
-    const { title, details, maximumAttendees } = request.body;
-
-    const slug = generateSlug(title);
-
-    const eventWithSameSLug = await prisma.event.findFirst({
-      where: {
-        slug,
-      },
-    });
-
-    if (eventWithSameSLug !== null) {
-      throw new Error("Event with same slug already exists");
-    }
-
-    const event = await prisma.event.create({
-      data: {
-        title,
-        details,
-        maximumAttendees,
-        slug,
-      },
-    });
-
-    return reply.status(201).send({ eventId: event.id });
-  }
-);
+app.register(createEvent);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running on port 3333");
